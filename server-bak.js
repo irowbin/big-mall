4b1fdf59d02c7379a039ce9558db1907be0e7e9f
robin <irowbin@gmail.com>
2021-04-06 19:14:29 +0545

    refactor

---
+++ server-bak.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const path = require('path')
+const app = express()
+
+if (process.env.MODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+// lets pass the secret key from env file.
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+if (process.env.MODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/client/build')))
+
+// this * route is to serve project on different page routes except root `/`
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, '/client/build/index.html'))
+  })
+}
+
+const port = process.env.PORT || 8080
+
+app.listen(port, error => {
+  if (error) throw error
+  console.log(`app is listening on port: ${port}`)
+})
+
+app.post('/payment', (req, res) => {
+  console.log(req.body)
+  const body = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currency: 'usd'
+  }
+  stripe.charges.create(body, (stripeEx, stripeRes) => {
+    res.status(stripeEx ? 500 : 200).send(stripeEx ? { error: stripeEx } : { success: stripeRes })
+  })
+})
+
