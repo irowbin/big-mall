2f124f8b5a77e1c7c3344af1a0c375d243b19eac
robin <irowbin@gmail.com>
2021-04-06 20:36:38 +0545

    refactor

--- server/server.js
+++ server/server.js
@@ -5,20 +5,20 @@ const path = require('path')
 const serverless = require('serverless-http')
 const app = express()
 
-if (process.env.MODE_ENV !== 'production') {
-  require('dotenv').config()
-}
+// if (process.env.MODE_ENV !== 'production') {
+//   require('dotenv').config()
+// }
 
 const router = express.Router()
 // lets pass the secret key from env file.
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+// const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
+// app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(cors())
 
 // if (process.env.MODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/build')))
+//   app.use(express.static(path.join(__dirname, '/build')))
 
 // this * route is to serve project on different page routes except root `/`
 router.get('/', function(req, res) {
@@ -38,17 +38,17 @@ router.get('/', function(req, res) {
 //   console.log(`app is listening on port: ${port}`)
 // })
 
-app.post('/payment', (req, res) => {
-  console.log(req.body)
-  const body = {
-    source: req.body.token.id,
-    amount: req.body.amount,
-    currency: 'usd'
-  }
-  stripe.charges.create(body, (stripeEx, stripeRes) => {
-    res.status(stripeEx ? 500 : 200).send(stripeEx ? { error: stripeEx } : { success: stripeRes })
-  })
-})
+// app.post('/payment', (req, res) => {
+//   console.log(req.body)
+//   const body = {
+//     source: req.body.token.id,
+//     amount: req.body.amount,
+//     currency: 'usd'
+//   }
+//   stripe.charges.create(body, (stripeEx, stripeRes) => {
+//     res.status(stripeEx ? 500 : 200).send(stripeEx ? { error: stripeEx } : { success: stripeRes })
+//   })
+// })
 app.use('/.netlify/functions/server', router)  // path must route to lambda
 module.exports = app
 module.exports.handler = serverless(app)
